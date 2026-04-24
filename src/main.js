f647f3a6ec1e14511aab3b82ed4594f4e0fbdce7
= <=>
2026-04-24 15:53:02 -0300

    Atualizando .env e gitignore

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.use(Vuex, lodash)
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios
-Vue.prototype.$urlApi = 'http://localhost:8010/api/'
+Vue.prototype.$urlApi = process.env.VUE_APP_API_URL
 
 var store = {
     state: {
